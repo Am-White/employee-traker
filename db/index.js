0725e97f16a2c9487deed06de3fc83d8b8042e25
littlefire <aybwhite@gmail.com>
2021-01-11 14:02:10 -0800

    added to db schema

--- db/index.js
+++ db/index.js
@@ -1,1 +1,31 @@
 const connection = require("./connection");
+
+module.exports = {
+    //GET
+    getDepartments() {
+        return connection.query("SELECT * FROM departments");
+    },
+    getRoles() {
+        return connection.query("SELECT * FROM roles");
+    },
+    getEmployees() {
+        return connection.query("SELECT * FROM employees");
+    },
+
+    //INSERT
+    insertDepartments(data) {
+        return connection.query("INSERT INTO departments SET ?", data);
+    },
+    insertRoles(data) {
+        return connection.query("INSERT INTO roles SET ?", data);
+    },
+    insertEmployees(data) {
+        return connection.query("INSERT INTO employees SET ? WHERE ?",
+        [{
+            id: data.employeeId
+        },
+        {
+            role_id: data.roleId
+        }]);
+    }
+}
