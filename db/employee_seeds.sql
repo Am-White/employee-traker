1790ec29c2515a5a99756fbcc9bfdf0201c0ba6d
littlefire <aybwhite@gmail.com>
2021-01-18 19:36:33 -0800

    adding in logo data

--- db/employee_seeds.sql
+++ db/employee_seeds.sql
@@ -6,5 +6,5 @@ VALUES ("HR"), ("Accounting"), ("Sales"), ("Engineering");
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Manager", 75000, 1);
 
-INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Amethyst", "White", 1, NULL);
--- index.js
+++ index.js
@@ -6,3 +6,21 @@ const cTable = require("console.table");
 
 const db = require("./db");
 const connection = require("./db/connection");
+
+console.log(
+    logo({
+        name: 'Employee Database',
+        font: 'Big',
+        lineChars: 15,
+        padding: 2,
+        margin: 3,
+        borderColor: 'blue',
+        logoColor: 'white',
+        textColor: 'blue',
+    })
+        .emptyLine()
+        .right('version 1.0')
+        .emptyLine()
+        .center('Create, track and update departments, roles and employees!')
+        .render()
+);
