8d7a5bc4c0abdade7e9ad2a86cefef97db7794d4
littlefire <aybwhite@gmail.com>
2021-01-18 18:45:48 -0800

    finishing seeds.sql

--- db/employee_seeds.sql
+++ db/employee_seeds.sql
@@ -3,3 +3,7 @@ INSERT INTO departments (name)
 VALUES ("HR"), ("Accounting"), ("Sales"), ("Engineering");
 
 INSERT INTO rolse (title, salary, department_id)
+VALUES ("Manager", 75000, 1);
+
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES ("Amethyst", "White", 1, NULL);
