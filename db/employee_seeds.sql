d35df7444060c1d910340f48e6384e6ae5106e4d
littlefire <aybwhite@gmail.com>
2021-01-18 18:47:28 -0800

    typeo fix in seeds

--- db/employee_seeds.sql
+++ db/employee_seeds.sql
@@ -1,8 +1,9 @@
 USE employee_tracker_db;
+
 INSERT INTO departments (name)
 VALUES ("HR"), ("Accounting"), ("Sales"), ("Engineering");
 
-INSERT INTO rolse (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Manager", 75000, 1);
 
 INSERT INTO employees (first_name, last_name, roles_id, manager_id)
